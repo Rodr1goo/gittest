aabea9c918d0a5e9f985cf86a37e753320905502
Rodrigo Flores <rodri_flores_1998@hotmail.com>
2022-03-13 14:18:47 -0300

    nuevo componente

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+const NavBar = () => {
+        return(
+            <nav className="navbar">
+                <img src='/logo192.png' alt='logo' className='logo'/>
+                <a className="links" href="#"> Inicio </a> 
+                <a className="links" href="#"> Productos </a> 
+                <a className="links" href="#"> Nosotros </a> 
+
+
+            </nav>
+        )
+    
+}
+
+export default NavBar
